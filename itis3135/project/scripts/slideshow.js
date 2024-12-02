7ac84a5d38de2bcc5668b1b0c33e69deaab86a7b
abdulirfan1 <143642660+abdulirfan1@users.noreply.github.com>
2024-12-01 21:03:07 -0500

    Project work

---
+++ itis3135/project/scripts/slideshow.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    let captions = []
+    
+    $('.thumbnails img').on('click', function () {
+        let index = $(this).data('index');
+        updateSlide(index);
+    });
+
+    $('#next-btn').on('click', function () {
+        let currentIndex = $('.slideshow img.active').index();
+        let nextIndex = (currentIndex + 1) % $('.slideshow img').length;
+        updateSlide(nextIndex);
+    });
+
+    $('#prev-btn').on('click', function () {
+        let currentIndex = $('.slideshow img').index($('.slideshow img.active'));
+        let prevIndex = (currentIndex - 1 + $('.slideshow img').length) % $('.slideshow img').length;
+        updateSlide(prevIndex);
+    });
+
+    function updateSlide(index) {
+        $('.slideshow img').removeClass('active').eq(index).addClass('active');
+
+        $('.thumbnails img').removeClass('active').eq(index).addClass('active');
+
+        $('.caption').text(captions[index]);
+    }
+});
